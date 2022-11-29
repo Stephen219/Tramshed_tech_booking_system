f5caa876662ac345c90493d80fe3472a5ddb9f50
Clevis Gikenyi <GikenyiC@cardiff.ac.uk>
2022-11-29 12:41:32 +0000

    allowed extension

--- db.py
+++ db.py
@@ -1,6 +1,7 @@
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.sql import func
 import nanoid
+ALLOWED_EXTENXIONS= set(['txt','pdf','png','jpg','jpeg','gif'])
 
 db = SQLAlchemy()
 # !Migration steps!
